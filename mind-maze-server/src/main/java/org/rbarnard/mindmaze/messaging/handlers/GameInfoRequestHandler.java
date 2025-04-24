1a99661908cf9e855cc5f5c4a351f50607164958
RMBarnard <rbarnard1107@gmail.com>
2025-04-23 19:38:46 -0600

    Implmement game info broadcast and receive

--- mind-maze-server/src/main/java/org/rbarnard/mindmaze/messaging/handlers/GameInfoRequestHandler.java
+++ mind-maze-server/src/main/java/org/rbarnard/mindmaze/messaging/handlers/GameInfoRequestHandler.java
@@ -8,7 +8,10 @@ import org.rbarnard.mindmaze.GameRegistry;
 import org.rbarnard.mindmaze.SessionRegistry;
 import org.rbarnard.mindmaze.messaging.GameInfoRequest;
 import org.rbarnard.mindmaze.messaging.LobbyInfoResponse;
+import org.rbarnard.mindmaze.messaging.Message;
+import org.rbarnard.mindmaze.messaging.MessageType;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 
 import jakarta.websocket.Session;
@@ -17,6 +20,7 @@ public class GameInfoRequestHandler implements MessageHandler<GameInfoRequest> {
     private final GameRegistry gameRegistry;
     private final SessionRegistry sessionRegistry;
     private final BroadcastService broadcastService;
+    private final ObjectMapper objectMapper;
 
     @Inject
     public GameInfoRequestHandler(GameRegistry gameRegistry, SessionRegistry sessionRegistry,
@@ -24,10 +28,12 @@ public class GameInfoRequestHandler implements MessageHandler<GameInfoRequest> {
         this.gameRegistry = gameRegistry;
         this.sessionRegistry = sessionRegistry;
         this.broadcastService = broadcastService;
+        this.objectMapper = new ObjectMapper();
     }
 
     public void handle(GameInfoRequest gameInfoRequest, Session session) {
         Game game = gameRegistry.getGame(gameInfoRequest.getJoinCode());
+        Message response = new Message();
         if (game.isRunning()) {
             // TODO: Send some response for spectating or saying to try later
         } else {
@@ -36,6 +42,13 @@ public class GameInfoRequestHandler implements MessageHandler<GameInfoRequest> {
                     .toList();
             LobbyInfoResponse lobbyInfoResponse = new LobbyInfoResponse(playerIds, game.getShortId(),
                     game.getMapSize());
+            response.setTypeId(MessageType.GAME_INFO_RESPONSE.getTypeId());
+            try {
+                response.setPayloadJson(objectMapper.writeValueAsString(lobbyInfoResponse));
+            } catch (Exception e) {
+                // TODO: Handle
+            }
         }
+        broadcastService.broadcastToPlayer(response, session);
     }
 }
